7d04f4d5be747219c982f045a5ee8e7eeb363e78
olehaqa <>
2024-10-27 15:10:49 +0100

    Update bigo.py
    big_o_of_one

--- bigo.py
+++ bigo.py
@@ -3,12 +3,14 @@ def big_o(n: int):
     for x in range(n):
         print(x)
 
+
 #O(n^2) - n in square
 # Less efficient in comparison to big_o in terms of time complexity
 def big_osquare(n: int):
     for x in range(n):
         for y in range(n):
             print(x, y)
+
 
 # Drop Non Dominance
 # Operations can be calculated as: O(n^2) + O(n) OR O(n^2 + n).
@@ -22,13 +24,21 @@ def big_o_nondominance(n: int):
             print(x, y)
     # O(n)
     for a in range(n):
-            print(a)
+        print(a)
+
+
+# 0(1) - case when N is increasing but the number of operations stays the same aka constant time.
+# It does not matter if we have more sum operations like: n + n + n + etc
+# This is the most efficient Big O as N increases but number of operations static.
+def big_o_of_one(n: int):
+    return n + n
 
 
 if __name__ == '__main__':
     # O(n) - O(10)
     # big_o(n=10)
     big_osquare(n=10)
+    big_osquare(n=10)
+    big_o_of_one(n=2)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
