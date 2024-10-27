f14f8f3dd805bbe1918e52acb693951a9840df8c
olehaqa <>
2024-10-27 14:56:43 +0100

    Update bigo.py
    big_o_nondominance

--- bigo.py
+++ bigo.py
@@ -10,6 +10,20 @@ def big_osquare(n: int):
         for y in range(n):
             print(x, y)
 
+# Drop Non Dominance
+# Operations can be calculated as: O(n^2) + O(n) OR O(n^2 + n).
+# n^2 - is dominante term and n - is not.
+# As second n can become large with time and have it stand alone does not make any sense, and
+# it can be dropped.
+def big_o_nondominance(n: int):
+    #O(n^2)
+    for x in range(n):
+        for y in range(n):
+            print(x, y)
+    # O(n)
+    for a in range(n):
+            print(a)
+
 
 if __name__ == '__main__':
     # O(n) - O(10)
