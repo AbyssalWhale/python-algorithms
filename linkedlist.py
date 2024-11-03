d4509220b0f72d598a9f5594dbce63a98dd0cb9d
olehh <>
2024-11-03 14:02:03 +0100

    list vs linked list

--- linkedlist.py
+++ linkedlist.py
@@ -1,8 +1,10 @@
-def hello_world():
-    print("Hello!")
+def list_vs_linkedlist():
+    print("Linked List does not have indexes")
+    print("Linked List items are not located next to each other in memory. Each item is referencing to next item")
+    print("Linked List. Head - first item, tail - last item")
 
 
 if __name__ == '__main__':
     # O(n) - O(10)
     # big_o(n=10)
-    hello_world()
+    list_vs_linkedlist()
