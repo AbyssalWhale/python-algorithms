8e87814a252aabc93372be24f20cb7a78491dd3e
olehh <>
2024-11-03 14:38:18 +0100

    what_is_node

--- linkedlist.py
+++ linkedlist.py
@@ -6,6 +6,7 @@ RESET = "\033[0m"
 
 
 def list_vs_linkedlist():
+    print("-" * 10, "LIST VS LINKED LIST", "-" * 10)
     data = [
         ["append", f"{GREEN}O(1){RESET}", f"{GREEN}O(1){RESET}"],
         ["pop", f"{RED}O(n){RESET}", f"{GREEN}O(1){RESET}"],
@@ -19,13 +20,31 @@ def list_vs_linkedlist():
 
     headers = ["Big O", "Linked List", "List"]
     print(tabulate(data, headers))
-    print("-" * 10)
+    print("-" * 15)
     print("Linked List does not have indexes")
     print("Linked List items are not located next to each other in memory. Each item is referencing to next item")
     print("Linked List. Head - first item, tail - last item")
 
 
+def what_is_node():
+    print("-" * 10, "Node", "-" * 10)
+    print("- each item in linked list is node")
+    print("- node - its value and reference to next item (arrays)")
+    print("- linked list - is a collection of nodes/arrays")
+    example = {
+        "value": 1,
+        "next": {
+            "value": 2,
+            "next": {
+                "value": 3,
+                "next": None
+            }
+        }
+    }
+    print(f"example {example}")
+
+
+
 if __name__ == '__main__':
-    # O(n) - O(10)
-    # big_o(n=10)
     list_vs_linkedlist()
+    what_is_node()
